8164a3db368a865c234afc0aa7007821619fb36f
Tommaso Coricelli <coricellitommaso@gmail.com>
2024-07-07 15:15:19 +0200

    bakcground fix

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function playTrack(track) {
     var audio = new Audio('assets/' + track);
-    audio.play();
+    audio.play().catch(error => {
+        console.error("Error playing the audio file:", error);
+    });
 }
